a3defddc0565e840aeb902dce4f85a8fd89bc284
Justin Caldicott <justin@bluebirdtechnology.com>
2013-07-10 19:41:00 +0100

    Initial version.

---
+++ AndroidManifestVersioner/AndroidManifestVersioner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AndroidManifestVersioner
+{
+    public sealed class AndroidManifestVersioner
+    {
+        private readonly string _path;
+        private readonly int _versionCode;
+        private readonly string _versionName;
+
+        private static readonly Regex VersionCodeRegex = new Regex( "android:versionCode=\"(?<version>.*)\"", RegexOptions.Compiled );
+        private static readonly Regex VersionNameRegex = new Regex( "android:versionName=\"(?<version>.*)\"", RegexOptions.Compiled );
+
+        public AndroidManifestVersioner( string path, int versionCode, string versionName )
+        {
+            _path = path;
+            _versionCode = versionCode;
+            _versionName = versionName;
+        }
+
+        public void Execute()
+        {
+            using( var manifestFileStream = File.Open( _path, FileMode.Open, FileAccess.ReadWrite ) )
+            using( var streamReader = new StreamReader( manifestFileStream ) )
+            {
+                var manifestFileText = streamReader.ReadToEnd();
+
+                if( VersionCodeRegex.Match( manifestFileText ).Success )
+                    manifestFileText = VersionCodeRegex.Replace( manifestFileText, "android:versionCode=\"" + ( _versionCode ) + "\"", 1 );
+                else
+                    throw new InvalidOperationException( "Could not find versionCode section of AndroidManifest.xml." );
+
+                if( VersionNameRegex.Match( manifestFileText ).Success )
+                    manifestFileText = VersionNameRegex.Replace( manifestFileText, "android:versionName=\"" + ( _versionName ) + "\"", 1 );
+                else
+                    throw new InvalidOperationException( "Could not find versionName section of AndroidManifest.xml." );
+
+                using( var streamWriter = new StreamWriter( manifestFileStream ) )
+                {
+                    manifestFileStream.Seek( 0, SeekOrigin.Begin );
+                    streamWriter.Write( manifestFileText );
+                    manifestFileStream.SetLength( manifestFileText.Length );
+                }
+            }
+        }
+    }
+}
---
+++ AndroidManifestVersioner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AndroidManifestVersioner
+{
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            if( args.Length == 0 )
+            {
+                Console.WriteLine( "Android Manifest Versioner.\r\nusage: AndroidManifestVersioner <path> <versionCode> <versionName>.\r\n" );
+                return;
+            }
+
+            var versioner = new AndroidManifestVersioner( args[0], int.Parse(args[1]), args[2] );
+            versioner.Execute();
+        }
+    }
+}
